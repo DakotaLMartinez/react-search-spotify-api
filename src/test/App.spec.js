4251706a3b8efef9031762c0d6dc7ce9a7f7ca5b
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-27 17:31:06 -0700

    initial commit

---
+++ src/test/App.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+
+import App from '../components/App';
+
+describe('App', () => {
+  
+});
