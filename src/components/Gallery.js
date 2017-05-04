3eebc718e9e1b8717c6d41270140d6ae47977fa4
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-05-03 17:14:45 -0700

    adds play puase functionality

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,10 +2,92 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Gallery extends Component {
-  // add a constructor for state using 'reactstate'
+  constructor(props) {
+    super(props)
+    this.state = {
+      playing: false,
+      playingUrl: null, 
+      audio: null, 
+      currentTime: 0
+    }
+  }
   static propTypes = {
     tracks: PropTypes.array,
   };  
+
+  componentDidMount() {
+    this.interval = setInterval(this.isPlaying.bind(this), 8000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  isPlaying() {
+    if(!this.state.playing) {
+      return false;
+    } else if(this.state.audio && this.state.audio.currentTime >= 30) {
+      this.resetPlayer();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  handlePlayClick(previewUrl) {
+    if(this.isPlaying()) {
+      if(this.state.playingUrl === previewUrl) {
+        this.pausePlayback();
+      } else {
+        this.resetPlayer();
+        this.startPlaying(previewUrl);
+      }
+    } else {
+      this.startPlaying(previewUrl);
+    }
+  }
+
+  pausePlayback() {
+    this.state.audio.pause();
+    this.setState({
+      playing: false
+    });
+  }
+
+  resetPlayer() {
+    let audio = this.state.audio;
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = null;
+    this.setState({
+      playing: false,
+      playingUrl: null, 
+      audio
+    });
+  }
+
+  startPlaying(url) {
+    if(this.state.playingUrl === url) {
+      this.restartPlayback();
+    } else {
+      let audio = this.state.audio ? this.state.audio : new Audio(url);
+      audio.src = url;
+      audio.currentTime = 0;
+      this.setState({
+        playing: true, 
+        playingUrl: url, 
+        audio
+      });
+      audio.play();
+    }
+  }
+
+  restartPlayback() {
+    this.state.audio.play();
+    this.setState({
+      playing: true
+    });
+  }
   
   render() {
     return (
@@ -17,7 +99,8 @@ class Gallery extends Component {
                   <div className="relative ma1 aspect-ratio-ns aspect-ratio--1x1-ns">
                     <img
                       className="album-art"
-                      src={track.album.images[1].url} />
+                      src={track.album.images[1].url} 
+                      onClick={() => this.handlePlayClick(track.preview_url)}/>
                     <span 
                       className="track-name db f7 small w-100 tc bg-black o-70 absolute top-0"
                       style={{fontSize: '0.6rem'}}>
@@ -35,7 +118,5 @@ class Gallery extends Component {
 Gallery.defaultProps = {
   tracks: []  
 };
-
-// for docs on prop type validations type `reactvalidateproptypedocs`
 
 export default Gallery;
--- src/test/Gallery.spec.js
+++ src/test/Gallery.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
-
+import sinon from 'sinon';
 
 import Gallery from '../components/Gallery';
 import beatlesTracks from './fixtures/beatlesTracks';
@@ -36,6 +36,86 @@ describe('Gallery', () => {
 
   it('displays the name of the song', () => {
     expect(wrapper.find('.track').first().text()).to.include("Here Comes The Sun - Remastered")
+  });
+    
+  it('clicking the album art for a track calls playTrack(previewUrl)', () => {
+    const playTrack = sinon.spy(wrapper.instance(), 'handlePlayClick');
+    wrapper.find('.album-art').first().simulate('click');
+    expect(playTrack.calledWith('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null')).to.equal(true);
+  });
+
+  it('clicking the album art for a track plays the track', () => {
+    const audio = new Audio('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null')
+    wrapper.find('.album-art').first().simulate('click');
+    expect(wrapper.state().playing).to.equal(true);
+    expect(wrapper.state().playingUrl).to.equal('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null');
+    expect(wrapper.state().audio.src).to.equal(audio.src);
+  });
+
+  describe('while a track is playing', () => {
+    let audio, clock;
+
+    beforeEach(() => {
+      clock = sinon.useFakeTimers();
+      audio = new Audio('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null')
+      wrapper.find('.album-art').first().simulate('click');
+    });
+
+    it('clicking the album art for the playing track pauses playback', () => {
+      let pause = sinon.spy(wrapper.state().audio, 'pause');
+      wrapper.find('.album-art').first().simulate('click');
+      expect(wrapper.state().playing).to.equal(false);
+      expect(wrapper.state().playingUrl).to.equal('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null');
+      expect(wrapper.state().audio.src).to.equal('https://p.scdn.co/mp3-preview/6902e7da51d2f17e5369d57dadf8ce7d2a123f99?cid=null');
+      expect(pause.calledOnce).to.equal(true);
+    });
+
+    it('continues playback from previous position after play and pause', () => {
+      let audio = wrapper.state().audio;
+      audio.currentTime = 4;
+      wrapper.setState({currentTime: 4});
+      wrapper.find('.album-art').first().simulate('click');
+      wrapper.find('.album-art').first().simulate('click');
+      expect(wrapper.state().audio.currentTime).to.equal(4);   
+    });
+
+    it('clicking the album art for another track shifts playback to the new track', () => {
+      let audio = wrapper.state().audio;
+      audio.currentTime = 4
+      wrapper.find('.album-art').at(1).simulate('click'); 
+      expect(wrapper.state().playing).to.equal(true);
+      expect(wrapper.state().playingUrl).to.equal('https://p.scdn.co/mp3-preview/83090a4db6899eaca689ae35f69126dbe65d94c9?cid=null');
+      expect(wrapper.state().audio.src).to.equal('https://p.scdn.co/mp3-preview/83090a4db6899eaca689ae35f69126dbe65d94c9?cid=null');
+      expect(wrapper.state().audio.currentTime).to.equal(0);  
+    });
+
+    it('pausing one track and starting another starts the other track at the beginning', () => {
+      let audio = wrapper.state().audio;
+      audio.currentTime = 4
+      wrapper.find('.album-art').first().simulate('click');
+      wrapper.find('.album-art').at(1).simulate('click'); 
+      expect(wrapper.state().playing).to.equal(true);
+      expect(wrapper.state().playingUrl).to.equal('https://p.scdn.co/mp3-preview/83090a4db6899eaca689ae35f69126dbe65d94c9?cid=null');
+      expect(wrapper.state().audio.src).to.equal('https://p.scdn.co/mp3-preview/83090a4db6899eaca689ae35f69126dbe65d94c9?cid=null');
+      expect(wrapper.state().audio.currentTime).to.equal(0);  
+    });
+
+    it('clicking the album art for another track pauses playback on the previous track', () => {
+      let audio = wrapper.state().audio;
+      let pause = sinon.spy(audio, 'pause');
+      wrapper.find('.album-art').at(1).simulate('click'); 
+      expect(pause.calledOnce).to.equal(true);
+    });
+
+    it('clicking the album art for the same track after it has completed starts playback at beginning', () => {
+      let audio = wrapper.state().audio;
+      audio.currentTime = 33;
+      clock.tick(33000);
+      wrapper.find('.album-art').first().simulate('click');
+      expect(wrapper.state().audio.currentTime).to.equal(0);
+    });
+
+    
   });
   
 });
